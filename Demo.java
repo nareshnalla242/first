1ffa3bb1d5c96b896847d7d1dab240aabe7bb02a
naresh <nareshnalla567@gmail.com>
2020-07-11 17:32:25 +0530

    sucess Demo

---
+++ Demo.java
@@ -0,0 +1,13 @@
+class Demo{
+public static void main(String[]args){
+ 
+
+
+System.out.println("SDBMSD");
+System.out.println("NMNBCMSN");
+System.out.println("KSDJCHSJ");
+         
+         
+
+     }
+  }
